5066c9bef0d9fb434852d8b6206f4c49fd5c8424
Patrice Tisserand <p.tisserand@gmail.com>
2020-07-27 19:46:51 +0200

    lib/src/github_login: fixed header

--- lib/src/github_login.dart
+++ lib/src/github_login.dart
@@ -109,7 +109,7 @@ class _JsonAcceptingHttpClient extends http.BaseClient {
   final _httpClient = http.Client();
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    request.headers['Acception'] = 'application/json';
+    request.headers['Accept'] = 'application/json';
     return _httpClient.send(request);
   }
 }
