2a921581553b030cffc0f1081dbd9e76227fe6c9
Patrice Tisserand <p.tisserand@gmail.com>
2020-07-27 20:39:49 +0200

    lib/github_oauth_credentials: added sample

---
+++ lib/github_oauth_credentials.sample.dart
@@ -0,0 +1,5 @@
+const githubClientId = 'APP CLIENT ID';
+const githubClientSecret = 'APP CLIENT SECRET';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
